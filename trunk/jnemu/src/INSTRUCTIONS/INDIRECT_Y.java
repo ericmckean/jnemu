0dd620e19656e0743dccb7fc3cc11a3e736a3c59
spike_logfile@yahoo.com <spike_logfile@yahoo.com@365d40f0-25da-11df-ab0a-9f3f633ae91d>
2010-06-16 07:26:06 +0000

    -fixed some opcode in the INDIRECT_X and INDIRECT_Y -implemented opcode ADC.

--- trunk/jnemu/src/INSTRUCTIONS/INDIRECT_Y.java
+++ trunk/jnemu/src/INSTRUCTIONS/INDIRECT_Y.java
@@ -2,12 +2,49 @@ package INSTRUCTIONS;
 
 import CPU.CPU_MEMORY;
 import CPU.CPU_REGISTER;
+import CPU.FLAG;
 
 public class INDIRECT_Y
 {
     public static void STA()
     {
-        CPU_MEMORY.write8Bit((ADDRESS.get8BitAddressOperand() + CPU_REGISTER.Y), CPU_REGISTER.A);
+        int addr, LSB, MSB;
+
+        LSB = CPU_MEMORY.fastRead8Bit(ADDRESS.get8BitAddressOperand());
+        MSB = CPU_MEMORY.fastRead8Bit(ADDRESS.get8BitAddressOperand() + 1);
+        addr = ((MSB << 8) | LSB) + CPU_REGISTER.Y;
+
+        CPU_MEMORY.write8Bit(addr, CPU_REGISTER.A);
         CPU_REGISTER.PC += 2;
+    }
+
+    public static int ADC()
+    {
+        int tmp, Value, oldAddr, newAddr, MSB, LSB, cycle = 0;
+
+        MSB = CPU_MEMORY.fastRead8Bit(ADDRESS.get8BitAddressOperand() + 1);
+        LSB = CPU_MEMORY.fastRead8Bit(ADDRESS.get8BitAddressOperand());
+        oldAddr = ((MSB << 8) | LSB);
+        newAddr = ((MSB << 8) | LSB) + CPU_REGISTER.Y;
+
+        Value = CPU_MEMORY.read8Bit(newAddr);
+        tmp = CPU_REGISTER.A + Value;
+        FLAG.CHECK_OVERFLOW(CPU_REGISTER.A, Value, tmp);
+        FLAG.CHECK_ZERO(tmp);
+        FLAG.CHECK_NEGATIVE(tmp);
+        FLAG.CHECK_CARRY(tmp);
+        CPU_REGISTER.A = tmp & 0xFF;
+
+        if(CPU_MEMORY.getPage(oldAddr) == CPU_MEMORY.getPage(newAddr))
+        {
+            cycle = 5;
+        }
+        else
+        {
+            cycle = 6;
+        }
+
+        CPU_REGISTER.PC += 2;
+        return cycle;
     }
 }
