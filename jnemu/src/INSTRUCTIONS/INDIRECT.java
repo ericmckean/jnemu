bfab23af9c873da8c0d7d249e6ea6e40fe3af907
spike_logfile@yahoo.com <spike_logfile@yahoo.com@365d40f0-25da-11df-ab0a-9f3f633ae91d>
2010-07-26 07:27:35 +0000

    -Corrections on JMP Indirect.

--- jnemu/src/INSTRUCTIONS/INDIRECT.java
+++ jnemu/src/INSTRUCTIONS/INDIRECT.java
@@ -7,10 +7,12 @@ public class INDIRECT
 {
     public static void JMP()
     {
-        int addr, LSB, MSB;
+        int addr, LSB, MSB, tmp1, tmp2;
 
-        LSB = CPU_MEMORY.read8Bit(ADDRESS.get16BitAddressOperand());
-        MSB = CPU_MEMORY.read8Bit(ADDRESS.get16BitAddressOperand() + 1);
+        tmp1 = ADDRESS.get16BitAddressOperand();
+        tmp2 = (ADDRESS.get16BitAddressOperand() + 1) & 0xff;
+        LSB = CPU_MEMORY.read8Bit(tmp1);
+        MSB = CPU_MEMORY.read8Bit(tmp2 | (tmp1 & 0xff00));
         addr = (MSB << 8) | LSB;
         CPU_REGISTER.PC = addr;
     }
